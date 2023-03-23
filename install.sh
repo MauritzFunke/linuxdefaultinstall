4ea9f6abc062d04fe267b4846b058bd7445588ae
Mauritz <mauritz.funke@gmail.com>
2023-03-23 12:40:18 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
-cd /root
-git clone https://github.com/MauritzFunke/linuxdefaultinstall.git
-mv linuxdefaultinstall/.zsh .
-mv linuxdefaultinstall/.nanorc .
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s $(which zsh)
-apt install powerline
+# I know this will be buggy if more than 1 user exists
+USER_NAME=$(ls /home)
+USER_DIR=/home/$USER_NAME
+
+# Check if zsh is installed
+if [ ! -f /bin/zsh ]; then
+    apt install zsh
+fi
+# Install oh my zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git $USER_DIR/.oh-my-zsh
+
+# Change default shell to zsh
+chsh -s $(which zsh) $USER_NAME
+
+# Install theme
+git clone https://github.com/MauritzFunke/linuxdefaultinstall.git /tmp/install
+mv /tmp/install/mauritz.zsh-theme $USER_DIR/.oh-my-zsh/themes
+mv /tmp/install/.zshrc $USER_DIR
+
+chown -R $USER_NAME:$USER_NAME $USER_DIR
